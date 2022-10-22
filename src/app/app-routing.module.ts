5f30803241373f6251139b151ccdabb5201893fe
wchristian84 <wade.christian@gmail.com>
2022-10-22 18:39:42 -0500

    set up initial app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CurrentMedsComponent } from './medications/current-meds/current-meds.component';
+import { PastMedsComponent } from './medications/past-meds/past-meds.component';
+import { FridayComponent } from './schedule/friday/friday.component';
+import { MondayComponent } from './schedule/monday/monday.component';
+import { SaturdayComponent } from './schedule/saturday/saturday.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { SundayComponent } from './schedule/sunday/sunday.component';
+import { ThursdayComponent } from './schedule/thursday/thursday.component';
+import { TuesdayComponent } from './schedule/tuesday/tuesday.component';
+import { WednesdayComponent } from './schedule/wednesday/wednesday.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/current-meds', pathMatch: 'full'},
+  {path: 'current-meds', component: CurrentMedsComponent},
+  {path: 'past-meds', component: PastMedsComponent},
+  {path: 'schedule', component: ScheduleComponent, children:[
+    {path: 'monday', component: MondayComponent},
+    {path: 'tuesday', component: TuesdayComponent},
+    {path: 'wednesday', component: WednesdayComponent},
+    {path: 'thursday', component: ThursdayComponent},
+    {path: 'friday', component: FridayComponent},
+    {path: 'saturday', component: SaturdayComponent},
+    {path: 'sunday', component: SundayComponent},
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
