066b62eb68d010a1e4937fb19362c599c462ecda
wchristian84 <wade.christian@gmail.com>
2022-10-23 18:56:05 -0500

    created form to add new med to past/current meds

---
+++ src/app/medications/add-med/add-med.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { ActivatedRoute } from '@angular/router';
+
+import { Medication } from '../medications.model';
+import { MedicationsService } from '../medications.service';
+
+@Component({
+  selector: 'app-add-med',
+  templateUrl: './add-med.component.html',
+  styleUrls: ['./add-med.component.css']
+})
+export class AddMedComponent implements OnInit {
+  addMedForm = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    dosage: new FormControl(null),
+    frequency: new FormControl(null),
+    benefits: new FormControl(null),
+    sideEffects: new FormControl(null),
+    startDate: new FormControl(null),
+    stopDate: new FormControl(null),
+    reasonStopped: new FormControl(null),
+  });
+
+  constructor(private medicationsService: MedicationsService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+
+  }
+
+  onFormSubmit() {
+     let newMed = new Medication(
+      this.addMedForm.value.name,
+      this.addMedForm.value.dosage,
+      this.addMedForm.value.frequency,
+      this.addMedForm.value.benefits,
+      this.addMedForm.value.sideEffects,
+      this.addMedForm.value.startDate,
+      this.addMedForm.value.stopDate,
+      this.addMedForm.value.reasonStopped
+      );
+
+    if (this.route.pathFromRoot.toString().includes('current-meds')) {
+      this.medicationsService.addCurrentMed(newMed);
+    } else {
+      this.medicationsService.addPreviousMed(newMed);
+    }
+    alert(`${this.addMedForm.value.name} saved!`)
+    this.addMedForm.reset();
+  }
+
+}
