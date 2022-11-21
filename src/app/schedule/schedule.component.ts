b6e4404ee7d7b76488b36e095bf769b8040d66ca
wchristian84 <wade.christian@gmail.com>
2022-11-20 20:06:48 -0600

    started setting up logic

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { endOfWeek, getDate, startOfWeek } from 'date-fns';
+import { Medication } from '../medications/medications.model';
+import { MedicationsService } from '../medications/medications.service';
 
 @Component({
   selector: 'app-schedule',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
+  today!: Date;
+  weekStart!: Date;
+  weekEnds!: Date;
+  dailyMeds: Medication[] = [];
+  weeklyMeds: Medication[] = [];
+  monthlyMeds: Medication[] = [];
+  current: Medication[] = [];
 
-  constructor() { }
+  constructor(private medicationsService: MedicationsService) { }
 
   ngOnInit(): void {
+    // Set the current date
+    this.today = new Date();
+    // Get the first date of the current week
+    this.weekStart = startOfWeek(this.today);
+    // Get the last date of the current week
+    this.weekEnds = endOfWeek(this.today);
+    // Get current meds
+    this.current = this.medicationsService.currentMeds.slice();
+
+    for (let medication of this.current) {
+      if (medication.frequency === 'Daily') {
+        this.dailyMeds.push(medication);
+        console.log(this.dailyMeds);
+      }
+      else if (medication.frequency === 'Weekly') {
+        this.weeklyMeds.push(medication);
+      }
+      else if (medication.frequency === 'Monthly') {
+        this.monthlyMeds.push(medication);
+      }
+    }
   }
 
 }
