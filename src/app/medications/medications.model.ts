c13d1f349f8f570c9858de86f26afccc868c5aca
wchristian84 <wade.christian@gmail.com>
2022-11-22 22:21:55 -0600

    updated model

--- src/app/medications/medications.model.ts
+++ src/app/medications/medications.model.ts
@@ -5,7 +5,12 @@ export class Medication {
     public frequency?: string,
     public date?: number,
     public day?: string,
-    public timeOfDay?: string[],
+    public timeOfDay?: {
+      Morning: boolean,
+      Midday: boolean,
+      Evening: boolean,
+      Night: boolean
+    },
     public benefits?: string,
     public sideEffects?: string,
     public startDate?: string,
