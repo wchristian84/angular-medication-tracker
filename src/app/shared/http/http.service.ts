0f6e6346cc432cc2801f3a5bda02e4549679e16e
wchristian84 <99511387+wchristian84@users.noreply.github.com>
2023-04-19 18:29:16 +0000

    setting variable in function instead of global

--- src/app/shared/http/http.service.ts
+++ src/app/shared/http/http.service.ts
@@ -21,23 +21,26 @@ export interface DeleteResponse{
 })
 export class HttpService {
   databaseURL = `${environment.apiRoute}medications/`;
-  user: UserData = JSON.parse(localStorage.getItem('userData') as string);
+  // user: UserData = JSON.parse(localStorage.getItem('userData') as string);
 
   constructor(private http: HttpClient) {}
 
   deleteFromDatabase(med_id: number) {
+    let user: UserData = JSON.parse(localStorage.getItem('userData') as string);
     let body = {
       med_id: med_id,
-      user_id: this.user.id
+      user_id: user.id
     };
     return this.http.delete<DeleteResponse>(`${this.databaseURL}delete`, {body: body});
   }
 
   fetchMedsFromDatabase() {
-    return this.http.get<ResponseData>(`${this.databaseURL}meds`, {params: {user_id: this.user.id}});
+    let user: UserData = JSON.parse(localStorage.getItem('userData') as string);
+    return this.http.get<ResponseData>(`${this.databaseURL}meds`, {params: {user_id: user.id}});
   }
 
   saveEditsToDatabase(editedMed: Medication) {
+    let user: UserData = JSON.parse(localStorage.getItem('userData') as string);
     let body = {
       id: editedMed.id,
       name: editedMed.name,
@@ -55,13 +58,14 @@ export class HttpService {
       midday: editedMed.midday,
       evening: editedMed.evening,
       night: editedMed.night,
-      user_id: this.user.id
+      user_id: user.id
     };
     return this.http.patch<ResponseData>(`${this.databaseURL}edit/`, body);
   }
 
   saveNewToDatabase(newMed: Medication) {
-    newMed.user_id = this.user.id;
+    let user: UserData = JSON.parse(localStorage.getItem('userData') as string);
+    newMed.user_id = user.id;
     return this.http.post<ResponseData>(`${this.databaseURL}new/`, newMed);
   }
 
